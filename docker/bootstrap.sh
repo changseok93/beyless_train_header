8e1f00a240cd0d8af538b3e580bfed322a8822b6
changseok <changseok93@icloud.com>
2020-11-03 01:03:48 +0000

    add tensorboard port forwarding (fix)

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -2,7 +2,7 @@ docker build --build-arg USER_ID=$UID -t detectron2_beyless_header:v0.1 .
 
 docker run --gpus all -it \
 	-e LC_ALL=C.UTF-8 \
-	-p 6006:6006
+	-p 6006:6006 \
 	--shm-size=8gb --env="DISPLAY" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
 	--name=detectron2_beyless_header detectron2_beyless_header:v0.1
 
