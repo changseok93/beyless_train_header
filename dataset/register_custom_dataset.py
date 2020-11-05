0ce1109b317b39bf504a66dd57ee9160a8e405bf
changseok <changseok93@icloud.com>
2020-11-05 09:12:17 +0900

    add catalog metadata to custom dataset

--- dataset/register_custom_dataset.py
+++ dataset/register_custom_dataset.py
@@ -11,6 +11,22 @@ def regist_custom_dataset(dataset_dir='/home/appuser/beyless_train_header/datase
         # BoxMode.XYWH_ABS -> 1
         register_coco_instances(name, {"bbox_mode":1}, json_file, image_root)
 
+    MetadataCatalog.get("custom_train").thing_classes = ["콜라", 
+            "몬스터", 
+            "박카스",        
+            "칠성사이다",
+            "파워에이드",
+            "토레타",
+            "광동옥수수수염차",
+            "펩시",
+            "cu블루레몬에이드",
+            "포도봉봉",
+            "갈아만든배",
+            "top더블랙",
+            "top마스터라떼",
+            "top스위트아메리카노",
+            "비타500"]
+
     custom_train_metadata = MetadataCatalog.get("custom_train")
     custom_val_metadata = MetadataCatalog.get("custom_val")
     custom_test_metadata = MetadataCatalog.get("custom_test")
